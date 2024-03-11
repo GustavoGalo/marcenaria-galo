45850eefa387b0d6773a8ff9c8c1c6961d301141
Gustavo Galo <gg.gustavo.galo@gmail.com>
2024-03-10 23:05:23 -0300

    fix: build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://gustavogalo.github.io',
   base: '/marcenaria-galo',
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
 });
