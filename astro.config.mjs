a48515b1d092c94e5f8969ff078b689b5f5eb338
Gustavo Galo <gg.gustavo.galo@gmail.com>
2024-03-10 22:50:45 -0300

    feat: add galeria page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://gustavogalo.github.io',
   base: '/marcenaria-galo',
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
