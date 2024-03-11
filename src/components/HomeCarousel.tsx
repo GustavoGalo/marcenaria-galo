200477f88e56cf11adc7c26a91c7c3b6a4ec6594
Gustavo Galo <gg.gustavo.galo@gmail.com>
2024-03-11 17:37:03 -0300

    fix: image display

--- src/components/HomeCarousel.tsx
+++ src/components/HomeCarousel.tsx
@@ -14,21 +14,21 @@ export default function HomeCarousel() {
           <img
             src="/marcenaria-galo/imgs/285064869_1320839168446484_3479823862929898784_n.jpg"
             alt=""
-            className="w-full md:max-h-[600px] max-h-96 object-cover"
+            className="w-full md:max-h-[600px] max-h-96 object-cover object-center m-auto"
           />
         </CarouselItem>
         <CarouselItem>
           <img
             src="/marcenaria-galo/imgs/284729798_3283231415259192_1610654909653121753_n.jpg"
             alt=""
-            className="w-full md:max-h-[600px] max-h-96 object-cover"
+            className="w-full md:max-h-[600px] max-h-96 object-cover object-center m-auto"
           />
         </CarouselItem>
         <CarouselItem>
           <img
             src="/marcenaria-galo/imgs/285109124_1879583948915332_3952310456083359289_n.jpg"
             alt=""
-            className="w-full md:max-h-[600px] max-h-96 object-cover"
+            className="w-full md:max-h-[600px] max-h-96 object-cover object-center m-auto"
           />
         </CarouselItem>
       </CarouselContent>
